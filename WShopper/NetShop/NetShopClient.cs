aa6ffb550f80c8b10c8b93fcce75ead2d140daa0
Redellex <masterpaw5679966@gmail.com>
2025-04-27 20:49:43 +0200

    Added dayshopper + fixes and unification to one model

--- WShopper/NetShop/NetShopClient.cs
+++ WShopper/NetShop/NetShopClient.cs
@@ -15,39 +15,44 @@ public class NetShopClient
         client = new RestClient(netShopConfig.BaseUrl);
     }
 
-    public async Task<List<NetShopAdModel>> QuerySearch(string query)
+    public async Task<List<AdModel>> QuerySearch(string query)
     {
         var request = new RestRequest(searchEndpoint + $"?query={query}&categories[]=");
         var response = await client.ExecuteAsync(request);
         return ExtractAds(response.Content);
     }
 
-    private List<NetShopAdModel> ExtractAds(string content)
+    private List<AdModel> ExtractAds(string content)
     {
         var doc = new HtmlDocument();
         doc.LoadHtml(content);
         var adNodes = doc.DocumentNode.SelectNodes("//div[contains(@id, 'ogloszenie-')]");
-        List<NetShopAdModel> ads = new List<NetShopAdModel>();
+        List<AdModel> ads = new List<AdModel>();
         foreach (var adNode in adNodes)
         {
+ 
             var titleNode = adNode.SelectSingleNode(".//div[@class='title']/h3");
-            string title = titleNode?.InnerText.Trim();
-
+            string title = titleNode != null? titleNode.InnerText.Trim() : "";
+            
             var priceNode = adNode.SelectSingleNode(".//div[@class='price']/span");
-            string price = priceNode?.InnerText.Trim();
+            string price = priceNode != null ? priceNode.InnerText.Trim() : "";
 
             var descriptionNode = adNode.SelectSingleNode(".//div[@class='description']/div[@class='info']/p");
-            string description = descriptionNode?.InnerText.Trim();
+            string description = descriptionNode != null ? descriptionNode.InnerText.Trim() : "";
             
             var linkNode = adNode.SelectSingleNode(".//a[@title]");
-            string link = linkNode?.GetAttributeValue("href", "");
+            string link = linkNode != null ? linkNode.GetAttributeValue("href", "") : "";
 
-            var ad = new NetShopAdModel
+            var ImageNode = adNode.SelectSingleNode(".//div[@class='thumb']/img");
+            var imageUrl  = ImageNode != null ? ImageNode.GetAttributeValue("src", "").Trim() : "";
+
+            var ad = new AdModel
             {
                 Title = title,
                 Price = price,
                 Description = description,
-                Link = link
+                Link = link,
+                Image = imageUrl
             };
 
             ads.Add(ad);
